a346cfb71d1a6cd78749ea5a8a7b139964d3d11b
skyhanullee <leeskyha@gmail.com>
2023-07-11 12:33:54 -0700

    got rid of 127.0.0.1 in console log

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URI, { dbName: `jobSearch` }).then(() => {
   server.listen(port, () => {
-    console.log(`Server is listening on http://127.0.0.1:${port}`);
+    console.log(`Server is listening on PORT:${port}`);
   });
 }).catch((e) => {
   console.error(`Failed to start server:`, e);
