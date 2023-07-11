155783eb6c26f17cda276d5f9a90bc425091ed0c
skyhanullee <leeskyha@gmail.com>
2023-07-11 12:36:27 -0700

    got rid of PORT in console log since it was not listening on a specific port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URI, { dbName: `jobSearch` }).then(() => {
   server.listen(port, () => {
-    console.log(`Server is listening on PORT:${port}`);
+    console.log(`Server is running!`);
   });
 }).catch((e) => {
   console.error(`Failed to start server:`, e);
